dc0608d621ad257fd6e267d71f42145707fd53b0
WillenSouza <souzawillen.dev@gmail.com>
2026-03-10 12:58:44 -0400

    Subindo aula 02
    Adicionando código da aula 02.

---
+++ fundamentos-do-python/variaveis_constantes.py
@@ -0,0 +1,9 @@
+nome = "Willen"
+idade = 22
+print(nome, idade)
+
+nome, idade = "Vitor", 21
+print(f"O nome do meu amigo é {nome} e ele fez {idade} anos hoje")
+
+DATA_NASCIMENTO = "10/03/2005"
+print(f"Parabéns Vitor, seu aniversário é hoje, você nasceu no dia {DATA_NASCIMENTO}")
