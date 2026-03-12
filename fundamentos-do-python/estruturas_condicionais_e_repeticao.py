28eb65aa09ab72c443512f1f860c88dd6e2effa5
WillenSouza <souzawillen.dev@gmail.com>
2026-03-12 14:35:35 -0400

    Update estruturas_condicionais_e_repeticao.py
    Atualizando arquivo com estruturas de repetição

--- fundamentos-do-python/estruturas_condicionais_e_repeticao.py
+++ fundamentos-do-python/estruturas_condicionais_e_repeticao.py
@@ -32,3 +32,49 @@ print(f"Resultado: {resultado}")
 resultado = "sucesso"
 validacao = resultado if resultado == "sucesso" else "Resultado negativo!"  # Operador ternário
 print(validacao)
+
+
+print("__________Estruturas de Repetição_________")
+
+VOGAIS = "AEIOU"
+palavra = input("Digite uma palavra: ")
+
+for letras in palavra:
+    if letras.upper() in VOGAIS:
+        print(f"{letras} é uma vogal!")
+
+valor = int(input("Digite um número para calcular a tabuada: "))
+
+for numero in range(0, 11):
+    resultado = numero * valor
+    print(f"{numero} x {valor} = {resultado}")
+
+
+print("Olá, bem vindo ao menu de opções!")
+print("Digite 1 para fazer uma transferência\n Digite 2 para ver o saldo \n Digite 0 para sair")
+opcao = -1
+while opcao != 0:
+    opcao = int(input("Digite a opção desejada: "))
+    if opcao == 1:
+        print("Você escolheu fazer uma transferência!")
+    elif opcao == 2:
+        print("Você escolheu ver o saldo!")
+    elif opcao == 0:
+        print("Saindo do menu, até mais!")
+    else:
+        print("Opção inválida, tente novamente!")
+
+
+print("Olá, bem vindo ao menu de opções!")
+print("Digite 1 para fazer uma transferência\n Digite 2 para ver o saldo \n Digite 0 para sair")
+opcao = -1
+while True:
+    opcao = int(input("Digite a opção desejada: "))
+    if opcao == 1:
+        print("Você escolheu fazer uma transferência!")
+    elif opcao == 2:
+        print("Você escolheu ver o saldo!")
+    elif opcao == 0:
+        break
+    else:
+        print("Opção inválida, tente novamente!")
