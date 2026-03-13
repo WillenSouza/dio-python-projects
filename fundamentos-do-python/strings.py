2775b4ba3779d5ccfe66bf7dd4bcb4332bba77b2
WillenSouza <souzawillen.dev@gmail.com>
2026-03-13 11:45:59 -0400

    Update strings.py
    Atualizando string.py com os conteúdos finais do módulo.

--- fundamentos-do-python/strings.py
+++ fundamentos-do-python/strings.py
@@ -16,3 +16,48 @@ print(texto.count("o"))  # 2
 
 print(".".join(texto))  # P.r.o.g.r.a.m.a.d.o.r
 print(texto.split("a"))  # ['Progr', 'm', 'dor']
+
+print("Interpolação de strings:".center(100, "_"))
+
+nome = "Willen"
+idade = 22
+email = "souzawillen.dev@gmail.com"
+
+print("Muito prazer, meu nome é %s, tenho %d anos e meu email é %s.\n\n" % (nome, idade, email))
+
+print("Muito prazer, meu nome é {}, tenho {} anos e meu email é {}.\n\n".format(nome, idade, email))
+
+print("Muito prazer, meu nome é {0}, tenho {1} anos e meu email é {2}.\n\n".format(nome, idade, email))
+
+print("Muito prazer, meu nome é {n}, tenho {i} anos e meu email é {e}.\n\n".format(n=nome, i=idade, e=email))
+
+print(f"Muito prazer, meu nome é {nome}, tenho {idade} anos e meu email é {email}.\n\n")
+
+PI = 3.14159
+print(f"O valor de PI é aproximadamente {PI:.2f}.")  # Delimitando o número de casas decimais para 2
+
+print("Fatiamento de strings:".center(100, "_"))
+
+nome = "Willen Souza"
+print(nome[0])  # W
+print(nome[1])  # i
+print(nome[0:6])  # Willen
+print(nome[:6])  # Willen
+print(nome[7:])  # Souza
+print(nome[-6:])  # Souza
+print(nome[::2])  # Wle oa
+print(nome[::-1])  # azuoS nelleW
+
+print("Strings multilinhas:".center(100, "_"))
+
+menu = """
+========================================
+    Menu:
+    1.Iniciar  sistema
+    2.Configurações
+    3.Sair
+
+=======================================
+"""
+
+print(menu)
